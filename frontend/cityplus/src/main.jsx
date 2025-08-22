f81e3b0cb175cb175beb4f107289e4027d8520a9
Rajeev.v <rajeevvalechha@gmail.com>
2025-08-22 10:36:37 +0530

    error fixed

--- frontend/cityplus/src/main.jsx
+++ frontend/cityplus/src/main.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  extendTheme as extendChakraTheme,
+} from "@chakra-ui/react";
+import {
+  ThemeProvider as MuiThemeProvider,
+  createTheme,
+} from "@mui/material/styles";
 import App from "./App.jsx";
-import "./index.css";
 
-const theme = extendTheme({});
+const chakraTheme = extendChakraTheme({});
+const muiTheme = createTheme();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
+    <ChakraProvider theme={chakraTheme}>
+      <MuiThemeProvider theme={muiTheme}>
+        <App />
+      </MuiThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
